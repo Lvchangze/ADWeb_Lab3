925da251e83dd6f72bea59ea604e70555e454b2d
Lvchangze <18302010026@fudan.edu.cn>
2021-06-09 21:37:03 +0800

    碰撞检测

--- server/index.js
+++ server/index.js
@@ -12,12 +12,14 @@ app.get('/', function (req, res) {
     res.send('<h1>Hello world</h1>');
 });
 
+//服务端接收每个客户端的实时信息（位置信息与旋转信息），并广播给其他客户端。
 io.on('connection', function (socket) {
     console.log('client '+ socket.id + ' connected');
     socket.on('player', function (data) {
         data.socketid = socket.id;
         socket.broadcast.emit('player', data);
     });
+    //当有某个客户端断开连接的时候，也要做一次广播。
     socket.on('disconnect', function () {
         console.log('client ' + socket.id + ' disconnected');
         socket.broadcast.emit('offline', {socketid: socket.id});
